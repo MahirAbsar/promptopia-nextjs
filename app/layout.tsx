a248f2aa169baaf6d293ba65fae07a663d76db13
Mahir <mahirabsar181@gmail.com>
2024-08-22 11:31:31 +0600

    chor: setup file folder structure

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Promptopia",
+  description: "Generate & Share AI prompts",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="main">
+          <div className="gradient"/>
+        </div>
+        <div className="app">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
